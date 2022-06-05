b20801251550bbc3eb37ae20dd9f4dff9f7f9d26
Paul <paul@pallen.me.uk>
2022-06-05 08:41:52 +0100

    Implement length less than filter

---
+++ Filters/Filters/LengthLessThanFilter.cs
@@ -0,0 +1,24 @@
+﻿namespace Filters.Filters
+{
+    public class LengthLessThanFilter : IWordFilter
+    {
+        private readonly int _maxLength;
+
+        public LengthLessThanFilter(int maxLength)
+        {
+            _maxLength = maxLength;
+        }
+
+        /// <summary>
+        /// Word length is less than our limit.
+        /// </summary>
+        /// <param name="word">The word.</param>
+        /// <returns>
+        /// true if word length is less than our limit
+        /// </returns>
+        public bool WordMatchesCondition(string word)
+        {
+            return word.Length < _maxLength;
+        }
+    }
+}
