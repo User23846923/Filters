a4ec9c8db215e6fed80d8c44bd685eae34f5285e
Paul <paul@pallen.me.uk>
2022-06-05 10:39:56 +0100

    Test and implement main processing loop

--- Filters/Program.cs
+++ Filters/Program.cs
@@ -3,34 +3,37 @@ using Filters.Services;
 
 namespace Filters
 {
-    internal class Program
+    /// <summary>
+    ///    Read from a txt file
+    /// • Create and apply the following 3 filters:
+    /// – Filter1 – filter out all the words that contains a vowel in the middle of the word – the centre 1 or 2 letters
+    /// ("clean" middle is ‘e’, "what" middle is ‘ha’, "currently" middle is ‘e’ and should be filtered, "the", "rather"
+    /// should not be)
+    /// – Filter2 – filter out words that have length less than 3
+    /// – Filter3 – filter out words that contains the letter ‘t’
+    /// • After all filters have completed display the resulted text in the console;
+    /// </summary>
+    public class Program
     {
-        private static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            //var seperator = "";
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usage: Filters fileName");
+                return;
+            }
 
-            var filterList = new FilterList(new List<IFilter> {
-                new LengthFilter(4),
+            var filterList = new AnyFilterList(new List<IWordFilter> {
+                new VowelInTheMiddleFilter(),
+                new LengthLessThanFilter(3),
+                new ContainsCharacterFilter('t'),
             });
 
-            var reader = new LineReader(args[0]);
-            var tokenizer = new Tokenizer(reader);
-            foreach (string? token in tokenizer.NextToken())
-            {
-                if (token == null)
-                {
-                    break;
-                }
-
-                if (token.Length > 1 && filterList.ShouldFilterOut(token))
-                {
-                    continue;
-                }
-
-                //Console.Write(seperator);
-                Console.WriteLine(token);
-                //seperator = " ";
-            }
+            var processor = new Processor();
+            processor.ProcessFile(
+                args[0], 
+                filterList, 
+                (s) => Console.Write(s));
         }
     }
 }
---
+++ Filters/Services/Processor.cs
@@ -0,0 +1,46 @@
+﻿using Filters.Filters;
+using Filters.Services;
+
+namespace Filters.Services
+{
+    public class Processor : IProcessor
+    {
+        public void ProcessFile(
+            string filePath,
+            AnyFilterList filterList,
+            Action<string> outputFn)
+        {
+            var reader = new LineReader(filePath);
+            var tokenizer = new Tokenizer(reader);
+
+            var separator = "";
+            foreach (var token in tokenizer.GetNextToken())
+            {
+                ProcessToken(token, filterList, outputFn, ref separator);
+            }
+        }
+
+        public void ProcessToken(
+            string token,
+            AnyFilterList filterList,
+            Action<string> outputFn,
+            ref string separator)
+        {
+            if (token.Length == 1 && Char.IsPunctuation(token[0]))
+            {
+                outputFn(token);
+                separator = " ";
+                return;
+            }
+
+            if (filterList.WordMatchesCondition(token))
+            {
+                return;
+            }
+
+            outputFn(separator);
+            outputFn(token);
+            separator = " ";
+        }
+    }
+}
