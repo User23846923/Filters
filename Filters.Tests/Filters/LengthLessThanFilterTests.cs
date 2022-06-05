cceee3c47a1497d691beccb1c9b457952b433837
Paul <paul@pallen.me.uk>
2022-06-05 08:41:27 +0100

    Unit tests for length less than

---
+++ Filters.Tests/Filters/LengthLessThanFilterTests.cs
@@ -0,0 +1,34 @@
+using Filters.Filters;
+using NUnit.Framework;
+
+namespace Filters.Tests
+{
+    public class LengthLessThanFilterTests
+    {
+        [Test]
+        public void LengthEqualsReturnsFalse()
+        {
+            // Assemble
+            var filter = new LengthLessThanFilter(3);
+
+            // Act
+            var isMatching = filter.WordMatchesCondition("Ant");
+
+            // Assert
+            Assert.IsFalse(isMatching);
+        }
+
+        [Test]
+        public void LengthLessReturnsTrue()
+        {
+            // Assemble
+            var filter = new LengthLessThanFilter(4);
+
+            // Act
+            var isMatching = filter.WordMatchesCondition("Ant");
+
+            // Assert
+            Assert.IsTrue(isMatching);
+        }
+    }
+}
---
+++ Filters/Filters/IWordFilter.cs
@@ -0,0 +1,15 @@
+﻿namespace Filters.Filters
+{
+    /// <summary>
+    /// Interface for a filter for a single word.
+    /// </summary>
+    public interface IWordFilter
+    {
+        /// <summary>
+        /// Word matches our condition.
+        /// </summary>
+        /// <param name="word">The word.</param>
+        /// <returns>true if matches</returns>
+        bool WordMatchesCondition(string word);
+    }
+}
